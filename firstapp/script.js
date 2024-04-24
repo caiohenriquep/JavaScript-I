666d9c3a6540e17c2adc125a22c822b6e461f346
Caio Pedro <caiohenriquep@msn.com>
2024-04-24 19:21:30 -0300

    Continuacao do projeto JS

--- firstapp/script.js
+++ firstapp/script.js
@@ -1,12 +1,26 @@
+//constantes
+const SALARIO_ATE_20 = 1000
+const SALARIO_ACIMA_20 = 2000
+
 //input
-anoNascimento = 1994
-nome = "Caio"
+anoNascimento = parseInt(prompt("Informe o seu ano de nascimento:"))
+nome = prompt("Informe o seu nome:")
+salarioBase = parseFloat(prompt("Informe o seu salário base:"))
+gratificacao = parseFloat(prompt("Informe a sua gratificação:"))
+bonus = parseFloat(prompt("Informe o seu bônus:"))
+desconto = parseFloat(prompt("Informe o seu desconto:"))
+salarioLiquido = 0
+adicional = 0
 
 //processamento
 hoje = new Date()
 anoAtual = hoje.getFullYear()
 idade = anoAtual - anoNascimento
-mensagem = "Sou " + nome + " e tenho " + idade + " anos"
+
+adicional = 1000 //ou 2000
+
+salarioLiquido = salarioBase + gratificacao + bonus - desconto + adicional
 
 //output
+mensagem = "Sou " + nome + ", tenho " + idade + " anos e ganho R$" + salarioLiquido + "."
 alert(mensagem)
